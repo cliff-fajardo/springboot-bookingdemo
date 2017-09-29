53e62901c4166b48a4a0ebf7a59f99a0f5e13930
cfajardo <cliff.fajardo@gmail.com>
2017-09-28 22:29:24 -0700

    Add Angular + Change DB

--- src/main/java/com/cfajardo/bookingdemo/BookingRepository.java
+++ src/main/java/com/cfajardo/bookingdemo/BookingRepository.java
@@ -9,6 +9,10 @@ import java.util.List;
 @Repository
 public interface BookingRepository extends JpaRepository<HotelBooking, Long> {
 
+    /*
+        implementation done by Spring via convention
+        see: Sec. 3.4 - https://docs.spring.io/spring-data/jpa/docs/1.9.6.RELEASE/reference/html/
+     */
     List<HotelBooking> findByPricePerNightLessThan(double price);
 
 }
---
+++ src/main/resources/static/app/bookings.controller.js
@@ -0,0 +1,13 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('BookingsController', BookingsController);
+
+    BookingsController.$inject = [];
+
+    function BookingsController(){
+        var vm = this;
+    }
+})();
